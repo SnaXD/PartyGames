cdceb9a1da9029e093ae17c99cd7fdc7a19cf2b3
Jonathan T. Nielsen <44645695+SnaXD@users.noreply.github.com>
2022-06-07 08:13:22 +0200

    Initial Commit

---
+++ PartyGames/PartyGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyGamesApp.swift
+//  PartyGames
+//
+//  Created by Jonathan T. Nielsen on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PartyGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
